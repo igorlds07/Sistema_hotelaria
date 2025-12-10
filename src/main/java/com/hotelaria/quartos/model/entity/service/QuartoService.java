8a7769868e599d36dcea48b95a547ccdefa559e5
Igor Lopes <igorrrlopesss8@gmail.com>
2025-12-10 00:58:46 -0300

    Finalizado toda parte do Service do Quarto

--- src/main/java/com/hotelaria/quartos/model/entity/service/QuartoService.java
+++ src/main/java/com/hotelaria/quartos/model/entity/service/QuartoService.java
@@ -1,10 +1,20 @@
 package com.hotelaria.quartos.model.entity.service;
 
+import com.hotelaria.clientes.model.entity.ClienteEntity;
+import com.hotelaria.clientes.model.entity.repository.ClienteRepository;
 import com.hotelaria.quartos.model.entity.QuartoEntity;
 import com.hotelaria.quartos.model.entity.StatusQuarto;
+import com.hotelaria.quartos.model.entity.dto.QuartoOcuparDto;
+import com.hotelaria.quartos.model.entity.dto.QuartoResponseDto;
 import com.hotelaria.quartos.model.entity.repository.QuartoRepository;
+import com.hotelaria.usuario.model.entity.UsuarioEntity;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class QuartoService {
@@ -12,26 +22,44 @@ public class QuartoService {
     @Autowired
     private QuartoRepository quartoRepository;
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+
     public void iniciarQuartos(){
         if (quartoRepository.count() == 0){
             for (int i = 0; i < 15; i++){
                 QuartoEntity quarto = new QuartoEntity();
-                quarto.setNumeroQuarto(i);
+                quarto.setNumeroQuarto(i + 1);
                 quarto.setStatusQuarto(StatusQuarto.DISPONIVEL);
                 quartoRepository.save(quarto);
             }
         }
     }
-    public Integer ocuparQuarto(Integer numero){
+    public QuartoOcuparDto ocuparQuarto(Integer numero, Integer idCliente){
         QuartoEntity  quarto = quartoRepository.findByRoom(numero)
                 .orElseThrow(() -> new RuntimeException("Quarto não encontrado!"));
 
         if (quarto.getStatusQuarto() == StatusQuarto.OCUPADO){
             throw new RuntimeException("O quarto N°" + numero + " está ocupado!");
         }
-        quarto.setStatusQuarto(StatusQuarto.OCUPADO);
 
-        return quarto.getId();
+        ClienteEntity cliente = clienteRepository.findById(idCliente)
+                        .orElseThrow(() -> new RuntimeException("Cliente não enontrado!"));
+
+
+        quarto.setStatusQuarto(StatusQuarto.OCUPADO);
+        quarto.setOcupante(cliente);
+        quarto.setDataHoraEntrada(LocalDateTime.now());
+        quarto.setDataHoraSaida(null);
+        quartoRepository.save(quarto);
+
+        QuartoOcuparDto dto = new QuartoOcuparDto();
+        dto.setNumeroQuarto(quarto.getNumeroQuarto());
+        dto.setNomeCliente(quarto.getOcupante().getNome());
+        dto.setStatus(quarto.getStatusQuarto());
+        dto.setDataHoraEntrada(quarto.getDataHoraEntrada());
+
+        return dto;
     }
 
     public Integer desocuparQuarto(Integer numeroQuarto) {
@@ -42,10 +70,37 @@ public class QuartoService {
             throw new RuntimeException("O quarto N° " + numeroQuarto + " não está ocupado!");
         }
 
+        LocalDateTime saida = LocalDateTime.now();
+        quarto.setDataHoraSaida(saida);
+
+        if (quarto.getDataHoraEntrada() != null){
+            Duration duracao = Duration.between(quarto.getDataHoraEntrada(), saida);
+            long horas = duracao.toHours();
+        }
+
         quarto.setStatusQuarto(StatusQuarto.DISPONIVEL);
         quarto.setOcupante(null);
         quartoRepository.save(quarto);
 
         return quarto.getId();
+    }
+    public List<QuartoResponseDto> listarQuarto(){
+        return quartoRepository.findAll().stream()
+                .map(this::toResponse)
+                .toList();
+
+    }
+    private QuartoResponseDto toResponse(QuartoEntity quarto){
+        QuartoResponseDto dto = new QuartoResponseDto();
+        dto.setNumeroQuarto(quarto.getNumeroQuarto());
+        dto.setStatusQuarto(quarto.getStatusQuarto());
+        dto.setOcupante(quarto.getOcupante() != null ? quarto.getOcupante().getNome() : "Nenhum");
+
+        return dto;
+    }
+    public QuartoResponseDto buscarQuartoEspecifio(Integer numQuarto){
+        QuartoEntity quarto = quartoRepository.findByRoom(numQuarto)
+                .orElseThrow(() -> new RuntimeException("Quarto não encontrado"));
+        return toResponse(quarto);
     }
 }
