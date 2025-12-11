3317e4af5e9701795c15232e5431832fe3bd3aeb
Igor Lopes <igorrrlopesss8@gmail.com>
2025-12-11 00:42:09 -0300

    Modifiquei os campos da Entity
    Modifiquei o campo nomeCliente para IdCliente para eliminar a redundância de dados.

--- src/main/java/com/hotelaria/quartos/model/entity/dto/QuartoOcuparDto.java
+++ src/main/java/com/hotelaria/quartos/model/entity/dto/QuartoOcuparDto.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 @Data
 public class QuartoOcuparDto {
     private Integer numeroQuarto;
-    private String nomeCliente;
+    private Integer IdCliente;
     private LocalDateTime dataHoraEntrada;
     private StatusQuarto status;
 }
